aa63c1be72fc77717ed72c4764eccab4800825fc
Mark Tuscano <tuscanomark.mt@gmail.com>
2024-02-25 03:59:36 +0530

    updates

--- public/js/custom.js
+++ public/js/custom.js
@@ -72,3 +72,15 @@
   });
 
 })(jQuery); // End of use strict
+
+// google translate 
+           function googleTranslateElementInit() {
+               new google.translate.TranslateElement(
+                   {pageLanguage: 'en'},
+                   'google_translate_element'
+               );
+           }
+  <script type="text/javascript"
+               src=
+   "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
+       </script>
